f6398062fd6d4bb4bee04df7acc619ebc175b7fb
Emily <castillaemili3@gmail.com>
2026-05-15 21:52:01 -0500

    se hace corregimiento de errores

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import router from './router/index.js'
 
 // Bootstrap CSS y JS
 import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+
 
 // Bootstrap Icons
 import 'bootstrap-icons/font/bootstrap-icons.css'
