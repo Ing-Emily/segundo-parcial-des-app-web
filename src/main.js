241b7013da663bdde296bcec0f6de0be1715ad29
JHES561 <thejhes2006@gmail.com>
2026-05-19 10:47:35 -0500

    terminacion de union de bootstrap

--- src/main.js
+++ src/main.js
@@ -6,6 +6,10 @@ import router from './router'
 
 import './style.css'
 
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+
 import 'bootstrap-icons/font/bootstrap-icons.css'
 
 createApp(App)
